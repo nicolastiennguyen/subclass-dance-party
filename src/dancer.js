e437ca997bf5cee97869495340a08542553da82d
Qingzhou Yan <qingzhouyan@DESKTOP-B4UL0E9.localdomain>
2022-07-25 20:07:34 -0700

    in progress

--- src/dancer.js
+++ src/dancer.js
@@ -1,19 +1,64 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
 
+  // use jQuery to create an HTML <span> tag
+  this.$node = $('<span class="dancer"></span>');
+
+  this.step(timeBetweenSteps);
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+};
+
+makeDancer.prototype.step = function (timeBetweenSteps) {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(dancer.step, timeBetweenSteps);
+};
+
+makeDancer.prototype.setPosition = function (top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  dancer.$node.css(styleSettings);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Creates and returns a new dancer object that can step
+var makeDancer = function (top, left, timeBetweenSteps) {
+
   var dancer = {};
 
   // use jQuery to create an HTML <span> tag
   dancer.$node = $('<span class="dancer"></span>');
 
-  dancer.step = function() {
+  dancer.step = function () {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
     setTimeout(dancer.step, timeBetweenSteps);
   };
   dancer.step();
 
-  dancer.setPosition = function(top, left) {
+  dancer.setPosition = function (top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -30,3 +75,4 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+*/
